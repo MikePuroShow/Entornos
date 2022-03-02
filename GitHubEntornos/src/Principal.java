d71790250616502b600ffc58651f7fe91f397008
MikePuroShow <mgasolis@gmail.com>
2022-03-02 10:29:15 +0100

    Tercero

--- GitHubEntornos/src/Principal.java
+++ GitHubEntornos/src/Principal.java
@@ -2,8 +2,8 @@
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println("EEE");
-		System.out.println("jdjdj");
+
+		System.out.println("MODIFICACIONNNN");
 
 	}
 
